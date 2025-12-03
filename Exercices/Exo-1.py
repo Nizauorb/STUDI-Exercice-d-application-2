7435278867fb988b1efb110c56ba3df82a07021b
Nizauorb (Maxime Brouazin) <maxime.brouazin03@gmail.com>
2025-12-03 15:08:07 +0100

    Upload Exo1.py and Adding Pseudo Code

---
+++ Exercices/Exo-1.py
@@ -0,0 +1,7 @@
+nombre = int(input("Saisissez un nombre : "))
+
+# Vérifier si le nombre est pair ou impair
+if nombre % 2 == 0:
+    print("Le nombre est pair")
+else:
+    print("Le nombre est impair")
